20d86a14b986cbcf5a73213f697ee5c2bee4e17a
Elyeipi <Elyeipi@example.com>
2021-07-30 21:39:23 -0600

    Taking the videogames API

--- js/main.js
+++ js/main.js
@@ -1,34 +1,24 @@
-const aplicacion = document.querySelector('.container-date');
-const url = 'https://free-to-play-games-database.p.rapidapi.com/api/games';
-const listGames = [];
+const d = document;
+const w = window;
 
-fetch(url, {
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-key": "94c5c74304msh6b060db7c2a4341p1f01ecjsnddcc11168a4b",
-		"x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com"
+const API = 'https://free-to-play-games-database.p.rapidapi.com/api/games';
+const application = d.querySelector('.container-date');
+
+w.addEventListener('DOMContentLoaded', async (e) => {
+  try {
+    const res = await fetch(API, {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-key': '94c5c74304msh6b060db7c2a4341p1f01ecjsnddcc11168a4b',
+        'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com'
+      }
+    });
+    const json = await res.json();
+    console.log(json);
+    for(games of json){
+        application.innerHTML += `<div>Title: ${games.title} And id: ${games.id}</div>`;
     }
-})
-.then(date => {return date.json()})
-.then(games => {
-    listGames.push(games.map(item => {
-        const game = {
-            id: item.id,
-            title: item.title,
-            platform: item.platform,
-            developer: item.developer,
-            publisher: item.publisher,
-            release_date: item.release_date,
-            genre: item.genre,
-            description: item.short_description,
-            img: item.thumbnail,
-            freetogame_url: item.freetogame_profile_url,
-            game_url: item.game_url,
-        }
-        return game;
-    }));
-})
-.catch(error => console.log(error)) 
-
-console.log(listGames.length)
-
+  } catch (err) {
+    console.error(err);
+  }
+});
