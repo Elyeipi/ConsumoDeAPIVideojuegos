e2feba41784b8e75c44b4591690b076c8a82000a
Elyeipi <Elyeipi@example.com>
2021-08-01 11:24:29 -0600

    fetch of JP, similar to jochizan's fetch XDDDDD

--- js/main.js
+++ js/main.js
@@ -1,24 +1,34 @@
+'use strict';
+
+// constants 
 const d = document;
 const w = window;
-
+const containerDate = d.querySelector('.container-date');
 const API = 'https://free-to-play-games-database.p.rapidapi.com/api/games';
-const application = d.querySelector('.container-date');
 
-w.addEventListener('DOMContentLoaded', async (e) => {
-  try {
-    const res = await fetch(API, {
-      method: 'GET',
-      headers: {
-        'x-rapidapi-key': '94c5c74304msh6b060db7c2a4341p1f01ecjsnddcc11168a4b',
+// Fetch parameters
+const params = {
+    method: 'GET',
+    headers: {
         'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com'
-      }
-    });
-    const json = await res.json();
-    console.log(json);
-    for(games of json){
-        application.innerHTML += `<div>Title: ${games.title} And id: ${games.id}</div>`;
     }
-  } catch (err) {
-    console.error(err);
-  }
+};
+
+// Process
+
+w.addEventListener('DOMContentLoaded', async()=>{
+    try{
+        const request = await fetch(API, {
+            method: 'GET',
+            headers: {
+                'x-rapidapi-key': '94c5c74304msh6b060db7c2a4341p1f01ecjsnddcc11168a4b',
+                'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com'
+            }
+        });
+        const json = await request.json();
+    
+        
+    }catch(e){
+        console.log(e);
+    }
 });
